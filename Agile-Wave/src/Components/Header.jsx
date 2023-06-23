d963e4fc09924f7c057930d2ca3f30fa60b99063
Bella <emalibella45@gmail.com>
2023-06-23 20:33:19 +0300

    updated state for dropdown

--- Agile-Wave/src/Components/Header.jsx
+++ Agile-Wave/src/Components/Header.jsx
@@ -4,17 +4,18 @@ import upArrow from '../assets/upArrow.svg'
 import downArrow from '../assets/downArrow.svg'
 
 function Header() {
-
-  const [Dropdown , setDropdown]= useState(false);
-  const downArrowActive= () => {
-    setDropdown((state)=> !state)
+   const [Dropdown , setDropdown]= useState(false)
+   const downArrowActive= () => {
+  setDropdown((state)=> !state)
   }
+
+  
   return (
     <div className='bg-white p-4 fixed left-0 right-0 '>
       <header className='flex justify-between items-center'> 
       {/** To the left to the left */}
       <div className= 'flex items-center bg-white-300 space-x-2 md:space-x-4'>
-        <img src={logo} alt='logo'className='h-6 w-6'></img>
+        <img src={logo} alt='logo'className='w-10'></img>
         <h3 className=' hidden md:4-xl md:inline-block font-bold font-sans'>
           Agile Wave</h3>
         <div className='flex items-center'>
@@ -22,24 +23,26 @@ function Header() {
           '> 
             {/**board.name */}
           </h3>
-        
+          
           <img 
           src={ Dropdown? downArrow: upArrow}
           alt='Arrow down'
           className='w-3 ml-2 md:hidden'
           onClick={downArrowActive}
           />
+        
+          
         </div>
 
       </div>
 
       {/** To the right to the right */}
-      <div>
-        <button>
-
+      <div className ='flex space-x-3 items-center md:space-x-5'>
+        <button className='button hidden md:block'>
+        + New Task
         </button>
-        <button>
-
+        <button className=" button py-1 px-3 md:hidden ">
+          +
         </button>
       </div>
 
