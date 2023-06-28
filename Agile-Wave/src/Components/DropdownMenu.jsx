dfb6791785fb02e07377e2a86e63f31204e79dc2
Bella <emalibella45@gmail.com>
2023-06-28 17:31:51 +0300

    mobile -v  fetch boards success

--- Agile-Wave/src/Components/DropdownMenu.jsx
+++ Agile-Wave/src/Components/DropdownMenu.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-
+import boardIcon from '../assets/board.svg'
 function DropdownMenu({setdropDown}) {
 
     const boards = useSelector((state)=> state.boards)
     console.log(boards)
   return (
-    <div className='py-10 px-6  bg-blue-400 left-0 right-0 bottom-[-100vh] top-16 absolute'
+    <div className='py-10 px-6  bg-blue-200 left-0 right-0 bottom-[-100vh] top-20 absolute'
         onClick={(e)=> {
             if (e.target !== e.currentTarget){
             return
@@ -14,9 +14,20 @@ function DropdownMenu({setdropDown}) {
             setdropDown(false)
             console.log(e.currentTarget)
         }}>
-        <div className='bg-pink-300 w-full py-4 rounded-xl'>
+        <div className=' w-full py-4 rounded-xl bg-green-100'>
             <h3 className='
-            text-black-800 mx-4 mb-8'>All Boards</h3>
+            text-black-800 mx-4 mb-8'>All Boards: {boards?.length}</h3>
+            <div>
+                {boards.map((board, index)=> (
+                     <div key={index}
+                        className='flex py-2 '> 
+                        <img src={boardIcon} className='px-2' /> 
+                        <p className='px-2 text-lg'>{board.name}</p>
+                        </div>
+                )
+                   
+                )}
+            </div>
 
         </div>
     </div>
